255b79489c896780ba7566a0359a48bf79f0e01f
sandeep0119 <sandeepgupta1737@gmail.com>
2025-11-25 09:55:19 +0530

    task 1

---
+++ task1.py
@@ -0,0 +1,177 @@
+#1
+name="eshwar"
+age="22"
+company="aysqo"
+print(name,age,company)
+
+#2
+x=12
+y=1.55
+z= True
+name="eshwar"
+a = [1, 2, 4, 5,6]
+t=(1,2,3,4)
+d = {1:'eshwar',2:'chary',3:'jukanti','age':22}
+print(type(name))
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(d))
+print(type(t))
+print(x)
+print(y)
+print(z)
+print(a)
+print(d)
+print(t)
+print(name)
+
+#3
+a=10
+b=20
+a,b=b,a
+print(a)
+print(b)
+
+#4
+a="1234567890"
+print(a[-4:])
+
+#5
+a=10
+b=20
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print(c)
+print(d)
+print(e)
+print(f)
+
+#6
+a=10
+b=20
+c=30
+d=40
+e=50
+average=(a+b+c+d+e)/5
+print(average)
+
+#7
+a="eshwar"
+print(len(a))
+
+#8
+a=is_married = False
+print(type(a))
+
+#9
+x,y,z=5,10,15
+print(x,y,z)
+
+#10
+a=12345
+b=str(a)
+print(type(a))
+print(type(b))
+
+#11
+friuts=["apple","mango","banana","grapes","orange"]
+print(friuts[1],friuts[3])
+
+#12
+a={"name":"eshwar","age":22,"city":"Nizambad"}
+key,val=a.popitem()
+print(f"key:{key},value:{val}")
+
+#13
+a="hello python"
+b=" welcome to day1"
+c=a+b
+print(c)
+
+#14
+a="hello python"
+b=" welcome to day1"
+print(a,b)
+
+#15
+for i in range(1,5):
+    print("*" *i)
+
+#16
+a=("python is easy",)*5
+print(a)
+
+#17
+a=12
+b=23
+c=34
+print(a,b,c,sep="|")
+
+#18
+print("hello\tworld")
+
+#19
+print("he said \"python is best\".")
+
+#20
+name="eshwar"
+age="22"
+print(f"my name is {name} and my age is {age}")
+
+#21
+a="hello "
+b="ganesh"
+c=a+b
+print(c)
+
+#22
+a=50000
+print(f"my salary is {a}")
+
+#23
+a="hyderabad"
+print("my city is {}".format(a))
+
+#24
+name="eshwar"
+role="developer"
+company="aysqo"
+print(f"my name is {name} and my role is {role} at {company}")
+
+#25
+a=23.35956
+print("float value is : %.2f" %a)
+
+#26
+a="bharat ratna"
+b=2025
+print("i got {} award in {} year".format(a,b))
+
+#27
+current_year=2025
+print(f"current year is {current_year}")
+
+#28
+name="eshwar"
+age=22
+dept="software"
+print(name,age,dept,sep="|")
+
+#29
+a=("eshwar",)*3
+print(a)
+
+#30
+a="100"
+b=int(a)
+c=b*5
+print(f"the value of c is {c}")
+
+
+
+
+
